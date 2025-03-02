0be94d3a7ae87de9bce7b19968e4b4bac93cc905
Juuso Laine <juulain@gmail.com>
2025-03-02 14:41:12 +0200

    move event edit to correct path

--- src/features/layout/Footer.tsx
+++ src/features/layout/Footer.tsx
@@ -1,7 +1,12 @@
+import { routes } from "@/utils/routes";
+import Link from "next/link";
+
 export function Footer() {
   return (
-    <footer className="bg-gray-800 p-4 text-center text-white">
-      <p>© {new Date().getFullYear()}</p>
+    <footer className="bg-gray-800 p-4 text-center text-white flex flex-row justify-center gap-2">
+      <Link href={routes.auth.login}>Hallinta</Link>
+      <a href="https://athene.fi/hallinto/materiaalit/">Tietosuoja</a>
+      <a href="https://athene.fi">Athene.fi</a>
     </footer>
   );
 }
