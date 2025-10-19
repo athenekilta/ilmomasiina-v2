b893f79e290246fb043591371b38ed190aeaac9e
Eemil Aittokoski <86990190+eeait@users.noreply.github.com>
2025-10-19 15:55:50 +0300

    use user enums
    Co-authored-by: Olli Lohikoski <ollilohikoski@users.noreply.github.com>

--- src/server/router/users.ts
+++ src/server/router/users.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { router } from "../trpc/trpc";
 import { adminProcedure } from "../trpc/procedures/adminProcedure";
 import { TRPCError } from "@trpc/server";
+import { UserRole } from "@prisma/client";
 
 export const usersRouter = router({
   getUsers: adminProcedure.query(async ({ ctx }) => {
@@ -22,11 +23,11 @@ export const usersRouter = router({
     .input(
       z.object({
         userId: z.string(),
-        role: z.enum(["user", "admin"]),
+        role: z.nativeEnum(UserRole),
       }),
     )
     .mutation(async ({ input, ctx }) => {
-      if (input.userId === ctx.session.user.id && input.role !== "admin") {
+      if (input.userId === ctx.session.user.id && input.role !== UserRole.admin) {
         throw new TRPCError({
           code: "FORBIDDEN",
           message: "You can't demote yourself from admin role",
